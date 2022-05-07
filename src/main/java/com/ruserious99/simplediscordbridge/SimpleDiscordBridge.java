8a1a799ad7ca8d518881f734df4632f6a40c3993
lfind <lfindlay46@gmail.com>
2022-05-07 09:48:11 -0400

    romoverole command

--- src/main/java/com/ruserious99/simplediscordbridge/SimpleDiscordBridge.java
+++ src/main/java/com/ruserious99/simplediscordbridge/SimpleDiscordBridge.java
@@ -1,10 +1,13 @@
 package com.ruserious99.simplediscordbridge;
 
-import commands.GiveRole;
+import com.ruserious99.simplediscordbridge.commands.GiveRole;
+import com.ruserious99.simplediscordbridge.commands.RemoveRole;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
@@ -18,20 +21,27 @@ public final class SimpleDiscordBridge extends JavaPlugin {
 
         registerCommands();
 
-        JDABuilder builder = JDABuilder.createDefault("OTY3OTczNzkyNzY2ODkwMDk0.YmYF2A.kcmUkt446DlqbBMpEM1pxAC1onM");
+        JDABuilder builder = JDABuilder.create("OTY3OTczNzkyNzY2ODkwMDk0.YmYF2A.wfrKhqCZd-c1723mKlokm_k0fZU",
+                GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES);
         builder.setActivity(Activity.watching("You"));
         builder.setStatus(OnlineStatus.IDLE);
         builder.addEventListeners(new DiscordListener());
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGES);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+
         try {
             jda = builder.build();
-            System.out.println("Success");
         } catch (LoginException e) {
             e.printStackTrace();
         }
+        System.out.println("Success");
+
     }
 
     private void registerCommands() {
-        getCommand("give_role").setExecutor(new GiveRole(this));
+        getCommand("removerole").setExecutor(new RemoveRole(this));
+        getCommand("giverole").setExecutor(new GiveRole(this));
     }
 
     public JDA getJda() {
