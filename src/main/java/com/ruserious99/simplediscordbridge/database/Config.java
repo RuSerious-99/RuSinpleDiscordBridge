00e6e5c564f0df21e448b941605426750bf5ae1d
lfind <lfindlay46@gmail.com>
2022-06-04 16:27:29 -0400

    working on mc stuff

--- src/main/java/com/ruserious99/simplediscordbridge/database/Config.java
+++ src/main/java/com/ruserious99/simplediscordbridge/database/Config.java
@@ -2,6 +2,7 @@ package com.ruserious99.simplediscordbridge.database;
 
 public class Config {
 
+
     public final String HOST = "localhost";
     public final int PORT = 3306;
     public final String DATABASE = "tickets";
